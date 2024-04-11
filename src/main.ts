9aeaa109a148f5982c5281f7708be95760137042
Tagert <mini.andrius@gmail.com>
2024-04-11 19:59:23 +0300

    experimenting with scrolling animation

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,22 @@ const clickToSecondPage = () => {
 };
 
 findOut.addEventListener("click", clickToSecondPage);
+
+const sections = document.querySelectorAll(".section") as NodeListOf<HTMLDivElement>;
+console.log(sections);
+
+const checkScroll = () => {
+  sections.forEach((sec) => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 150;
+    const height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
+    }
+  });
+};
+
+window.addEventListener("scroll", checkScroll);
